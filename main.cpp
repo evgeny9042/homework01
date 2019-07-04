317459333d29ee09e296d572c2f3838d97e8b359
evgeny9042 <52294922+evgeny9042@users.noreply.github.com>
2019-07-04 12:20:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include "lib.h"
+
 int main() {
   std::cout << "HELLO OTUS!!!" << std::endl;
+  std::cout << "From library: " << my_library::version() << std::endl;
   return 0;
 }
